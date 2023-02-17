68a50c533fc59325caad9415e80e76ff6529fbf4
phufarpsw <phufa_rpsw@hotmail.com>
2023-02-17 14:54:10 +0700

    setup vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="bg-white px-4 py-2.5 flex border-b justify-between items-center">
+      <router-link class="space-x-2 flex items-center" to="/">
+        <img src="./assets/icons/image1.svg" />
+        <p>Cicero</p>
+      </router-link>
+    <div class="menu-navbar-group space-x-4 text-sm">
+      <router-link to="/men">MEN</router-link>
+      <router-link to="/women">WOMEN</router-link>
+      <router-link to="/kids">KIDS</router-link>
+      <router-link to="/sale">SALE</router-link>
+      <router-link to="/newin">NEW IN</router-link>
+    </div>
+    <div class="flex space-x-4">
+      <div class="search-bar bg-gray-200">
+      <img src="./assets/icons/Search_light@3x.svg" alt="">
+
+      </div>
+      <img src="./assets/icons/heart.svg" />
+      <img src="./assets/icons/basket.svg" />
+      <router-link to="/about">About</router-link>
+    </div>
   </nav>
   <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
   color: #2c3e50;
 }
 
