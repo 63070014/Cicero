ae9c6881cb2569583a5cd509f430c6316d82ac2d
phufarpsw <phufa_rpsw@hotmail.com>
2023-03-02 19:34:21 +0700

    link product on homepage

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -16,22 +16,22 @@
       <div class="flex w-full space-x-6 mt-10 cursor-pointer">
         <div class="listhomeProduct w-full" v-for="(item, index) in homeProduct" :key="index">
           <div class="w-full relative">
-            <img :src="item.url" />
+            <img :src="item[0].listImg[0]" @click="LinkTo('/productDetail/'+item[0].title)"/>
             <div class="p-2">
               <div class="flex items-center">
                 <img v-show="item.is_favourite == false" class="w-5 absolute right-0"
                   @click="item.is_favourite = !item.is_favourite" src="../assets/icons/heart.svg">
                 <img v-show="item.is_favourite == true" class="w-5 absolute right-0"
                   @click="item.is_favourite = !item.is_favourite" src="../assets/icons/heartt.svg">
-                <p class="text-md ">{{ item.title }}</p>
+                <p class="text-md">{{ item[0].title }}</p>
               </div>
-              <p class="text-2xl leading-5">{{ item.price }} <span class="text-sm text-gray-600">THB</span></p>
+              <p class="text-2xl leading-5">{{ item[0].price }} <span class="text-sm text-gray-600">THB</span></p>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="first-buyNowBanner mt-10 w-full h-full relative">
+      <div class="first-buyNowBanner mt-8 w-full h-full relative">
         <img src="../assets/homepage/buyNow1.svg" />
         <div
           class="w-44 h-14 bg-black text-white font-semibold center-set absolute top-10 right-10 cursor-pointer text-lg font-frans">
@@ -104,6 +104,11 @@ img {
 
 export default {
   name: "Homepage",
+  props:{
+        products:{
+            default: []
+        }
+    },
   components: {
   },
   data() {
@@ -114,11 +119,11 @@ export default {
         { name: "FOR KIDS", path: '/product/kids', url: require('../assets/homepage/forKids.png') }
       ],
       homeProduct: [
-        { title: "THE STAR CROP VEST", price: "1590", url: require('../assets/homepage/img1.svg'), is_favourite: false },
-        { title: "THE STAR CROP VEST", price: "1590", url: require('../assets/homepage/img2.svg'), is_favourite: false },
-        { title: "THE STAR CROP VEST", price: "1590", url: require('../assets/homepage/img3.svg'), is_favourite: false },
-        { title: "THE STAR CROP VEST", price: "1590", url: require('../assets/homepage/img4.svg'), is_favourite: false },
-        { title: "THE STAR CROP VEST", price: "1590", url: require('../assets/homepage/img5.svg'), is_favourite: false },
+        this.products.filter(e => e.id == '016'),
+        this.products.filter(e => e.id == '017'),
+        this.products.filter(e => e.id == '018'),
+        this.products.filter(e => e.id == '019'),
+        this.products.filter(e => e.id == '020'),
       ],
     }
   },
