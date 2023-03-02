fd0df2c91f8cc948053b10201bb4518f475dd188
63070014 <63070014@kmitl.ac.th>
2023-03-02 14:34:14 +0700

    signIn signUp Profile localStorage

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -31,33 +31,54 @@
         <div class="group-signin flex justify-center flex-col items-center mt-12">
             <div class=" flex justify-center items-center w-full">
                 <div class="sign-in w-2/4 text-center text-3xl">Sign in</div>
-                <div @click="LinkTo('/signup')" class="sign-up w-2/4 text-center text-3xl text-gray-200 cursor-pointer">Sign up</div>
+                <div @click="LinkTo('/signup')" class="sign-up w-2/4 text-center text-3xl text-gray-200 cursor-pointer" >Sign up</div>
             </div>
             <div class="flex flex-col justify-center w-full mt-8">
                 <span>EMAIL ADDRESS</span>
                 <div class="email mt-2">
-                    <input type="text" class="w-full bg-gray-200 h-9">
+                    <input type="text" class="w-full bg-gray-200 h-9 p-2" v-model="username">
                 </div>
                 <span class="mt-6">PASSWORD</span>
                 <div class="password mt-2">
-                    <input type="text" class="w-full bg-gray-200 h-9">
+                    <input type="text" class="w-full bg-gray-200 h-9 p-2" v-model="password">
                 </div>
-                <button @click="LinkTo('/')" class="bg-black text-white h-9 mt-8">LOGIN</button>
+                <button @click="checkMail()" class="bg-black text-white h-9 mt-8">LOGIN</button>
                 <span class="forgot mt-6 text-center cursor-pointer">Forgot your password ?</span>
             </div>
         </div>
-
     </div>
 </template>
 <script>
 export default {
+    username:'',
+    password:'',
+    getEmail:'',
+    getPass:'',
+    check:'',
     name: 'signin',
+    data() {
+        var getData = JSON.parse(localStorage.getItem('regisList'))
+        var getEmail = ''; var getPass = '';
+        for (let i = 0; i < getData.length; i++) {
+            getEmail += getData[i].localEmail
+            getPass += getData[i].localPassword
+        }
+            this.getEmail = getEmail
+            this.getPass = getPass
+    },
     methods: {
         homepage() {
             location.href='/'
         },
         LinkTo(whereTo){
             this.$router.push(whereTo)
+        },
+        checkMail(){
+            if(this.username == this.getEmail && this.password == this.getPass){
+                this.homepage()
+            }else{
+                alert("Your Email or Password is invalid")
+            }
         },
     }
 }
